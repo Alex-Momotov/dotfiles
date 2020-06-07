e1b57f8b283cf78b4acd96e805c9bbf055793b19
Alex Momotov <alex.momotov@vonage.com>
2020-06-07 14:02:23 +0100

    create_links.sh for VSCode

--- vscode/create_links.sh
+++ vscode/create_links.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ln -s /Users/omomotov/_projects_my/dotfiles/vscode/settings.json /Users/omomotov/Library/Application\ Support/Code/User/settings.json
 ln -s /Users/omomotov/_projects_my/dotfiles/vscode/keybindings.json /Users/omomotov/Library/Application\ Support/Code/User/keybindings.json
-ln -s /Users/omomotov/_projects_my/dotfiles/vscode/snippets/ /Users/omomotov/Library/Application\ Support/Code/User
+ln -s /Users/omomotov/_projects_my/dotfiles/vscode/snippets/ /Users/omomotov/Library/Application\ Support/Code/User/
 
 
