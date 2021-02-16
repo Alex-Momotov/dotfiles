ef5771f78733525006936cf0df8ee458ccb66d4e
Alex Momotov <alex.momotov@vonage.com>
2021-02-16 22:55:06 +0000

    mac settings.

--- mac_settings.sh
+++ mac_settings.sh
@@ -1,2 +1,3 @@
 defaults write -g InitialKeyRepeat -int 10 # Delay before key repeat
 defaults write -g KeyRepeat -int 1 # Key repeat speed
+defaults write -g com.apple.trackpad.scaling -float 5.0 # Trackpad speed
