68c64b7c22b8a4dab6e9f87cd51440bc0f590068
Alex Momotov <alex.momotov@vonage.com>
2021-02-17 01:12:15 +0000

    mac_settings

--- mac_settings.sh
+++ mac_settings.sh
@@ -1,3 +1,4 @@
 defaults write -g InitialKeyRepeat -int 10 # Delay before key repeat
 defaults write -g KeyRepeat -int 1 # Key repeat speed
 defaults write -g com.apple.trackpad.scaling -float 5.0 # Trackpad speed
+defaults write com.apple.finder QuitMenuItem -bool true # Able to quit Finder
